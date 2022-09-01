e45509414913f75a0d9e6e04cf98f364bb193d57
Troy Caselli <troycaselli@Troys-MacBook-Air.local>
2022-08-31 19:59:56 -0700

    completed Spinner.js component

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -37,23 +37,27 @@ STEP 4:
   Do you remember the operator we use to do "not"?
 */
 
-import React from 'react'; /* STEP 0 */
+import React, {useState} from 'react'; /* STEP 0 */
 
 export default function Spinner() {
 /* STEP 1 */
+const [spinnerOn, setSpinnerOn] = useState(true);
 
   const toggleSpinner = () => {
   /* STEP 4 */
+    // const spinner = document.querySelector('.spinner');
+    // spinnerOn === true ? spinner.style.display = 'none' : spinner.style.display = 'flex';
+    setSpinnerOn(!spinnerOn);
   };
 
   return (
     <div className='widget-spinner container'>
       <h2>Spinner</h2>
       {
-        true && <div id='spinner' className='spinner'>--+--</div> /* STEP 2 */
+        spinnerOn === true ? <div id='spinner' className='spinner'>--+--</div> : '' /* STEP 2 */
       }
       <button id='toggleSpinner' onClick={toggleSpinner}>
-        Hide Spinner {/* STEP 3 */}
+        {spinnerOn === true ? 'Hide Spinner' : 'Show Spinner'} {/* STEP 3 */}
       </button>
     </div>
   );
